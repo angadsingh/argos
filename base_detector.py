08268784e749d3ecd819da54b09ea6b2bed307dc
Angad Singh <angad@angadsingh.in>
2021-12-29 16:10:11 +0530

    minor bug fix

--- base_detector.py
+++ base_detector.py
@@ -8,9 +8,4 @@ class DetectorView(FlaskView):
     def apiconfig(self):
         self.config.fps_print_frames = int(request.args.get('fps_print_frames', self.config.fps_print_frames))
         self.config.tf_accuracy_threshold = float(request.args.get('tf_accuracy_threshold', self.config.tf_accuracy_threshold))
-        self.config.tf_detection_buffer_duration = int(
-            request.args.get('tf_detection_buffer_duration', self.config.tf_detection_buffer_duration))
-        self.config.tf_detection_buffer_threshold = int(
-            request.args.get('tf_detection_buffer_threshold', self.config.tf_detection_buffer_threshold))
-
         return Response(jsonpickle.encode(self.config.__dict__, unpicklable=False, max_depth=2), mimetype='application/json')
